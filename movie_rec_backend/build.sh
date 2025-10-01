b4b2d3eead8ecbc34c14f56485b75c8fab4a931a
frankiewilson1 <frankwilliamsugwu@gmail.com>
2025-10-01 23:28:13 +0100

    fix: bash script

--- movie_rec_backend/build.sh
+++ movie_rec_backend/build.sh
@@ -4,4 +4,7 @@ set -e
 pip install -r requirements.txt
 python manage.py collectstatic
 python manage.py migrate
+
+python manage.py shell -c "from movies.models import Movie; Movie.objects.all().delete()"
+
 python manage.py fetch_movies
